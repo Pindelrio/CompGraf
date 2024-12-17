532a76d5a8d4cde33e4373d82dc12fc33ce5ab16
Pin del Rio <Pindelrio@gmail.com>
2024-12-17 18:03:10 +0100

    2024-12-04_C11 Texture

--- CompGraf.cpp
+++ CompGraf.cpp
@@ -1,10 +1,10 @@
 #include <fstream>
 #include <iostream>
-#include <sstream>
 #include <windows.h>
 
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
+#include "stb/stb_image.h"
 
 
 const int width = 800;
@@ -13,37 +13,44 @@ const int height = 600;
 int customColorId;
 
 const char* vertexShaderSource =
-"#version 330 core\n"
-"\n"
-"layout (location = 0) in vec3 aPos;\n"
-"layout (location = 1) in vec3 aColor;\n"
-"out vec3 ourColor;\n"
-"\n"
-"void main()\n"
-"{\n"
-"    gl_Position = vec4(aPos,1.0);\n"
-"    ourColor = aColor;\n"
-"}\n\0";
+"#version 330 core                      \n"
+"                                       \n"
+"layout (location = 0) in vec3 aPos;    \n"
+"layout (location = 1) in vec3 aColor;  \n"
+"layout (location = 2) in vec2 aTex;    \n"
+"                                       \n"
+"out vec3 ourColor;                     \n"
+"out vec2 texCoord;                     \n"
+"                                       \n"
+"void main()                            \n"
+"{                                      \n"
+"    gl_Position = vec4(aPos,1.0);      \n"
+"    ourColor = aColor;                 \n"
+"    texCoord = aTex;                   \n"
+"}                                      \n\0";
 
 const char* fragmentShaderSource =
-"#version 330 core\n"
-"\n"
-"out vec4 FragColor;\n"
-"in vec3 ourColor;\n"
-"\n"
-"void main()\n"
-"{\n"
-"    FragColor = vec4(ourColor, 1.0f);\n"
-"};\n\0";
+"#version 330 core                      \n"
+"                                       \n"
+"out vec4 FragColor;                    \n"
+"in vec3 ourColor;                      \n"
+"in vec2 texCoord;                      \n"
+"uniform sampler2D tex0;                \n"
+"void main()                            \n"
+"{                                      \n"
+"    FragColor = texture(tex0, texCoord);\n"
+"};                                     \n\0";
 
-std::string ReadTextFile(const std::string& fileName);
 
 float vertices[] = {
-     0.0f, 0.5f, 0.0f,  0.0f, 0.0f, 1.0f,   
-    -0.5f,-0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   
-     0.5f,-0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   
+    //Coordenadas       //Color             //Textura (256x256 millor)
+     0.0f, 0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  0.5f, 0.0f, 
+    -0.5f,-0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  0.0f, 1.0f,    
+     0.5f,-0.5f, 0.0f,  0.0f, 0.0f, 1.0f,  1.0f, 1.0f,    
 };
-
+float indices[]={
+    0,1,2
+};
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
@@ -89,11 +96,6 @@ int main(void)
     glAttachShader(shaderProgram, fragmentShader);
     glLinkProgram(shaderProgram);
 
-    customColorId = glGetUniformLocation(shaderProgram, "alternativeColor");
-    
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-    
     unsigned int VAO;
     unsigned int VBO;
     unsigned int IBO;
@@ -106,34 +108,77 @@ int main(void)
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    float stride = 6 * sizeof(float);
-    void* offset = (void*)(3 * sizeof(float));
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    
+    GLsizei stride = 8 * sizeof(float);
+    void* offsetColor = (void*)(3 * sizeof(float));
+    void* offsetTexture = (void*)(6 * sizeof(float));
 
     glVertexAttribPointer(0,3, GL_FLOAT, GL_FALSE, stride, (void*)0);
     glEnableVertexAttribArray(0);
 
-    glVertexAttribPointer(1,3, GL_FLOAT, GL_FALSE, stride, offset);
+    glVertexAttribPointer(1,3, GL_FLOAT, GL_FALSE, stride, offsetColor);
     glEnableVertexAttribArray(1);
+
+    glVertexAttribPointer(2,2, GL_FLOAT, GL_FALSE, stride, offsetTexture);
+    glEnableVertexAttribArray(2);
     
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    // Texturas --------------------------------------------------------------------------------------------------------
+    int width, height, pixelSize;
+
+    //stbi_set_flip_vertically_on_load(true);
+    unsigned char* image = stbi_load("Images/Monkey.png", &width, &height, &pixelSize, 0);
+
+    GLuint textureID;
+    glGenTextures(1, &textureID);
+
+    glBindTexture(GL_TEXTURE_2D, textureID);
+
+    //Com es comporta el suavitzat de entre pixels
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    if (image)
+    {
+        if (pixelSize == 4)
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height,0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+        else
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height,0, GL_RGB, GL_UNSIGNED_BYTE, image);
+    }
+    else
+    {
+        std::cout << "Failed to load image" << std::endl;
+    }
+ 
+    stbi_image_free(image);
+    glBindTexture(GL_TEXTURE_2D, 0); //Evitem que la imatge es modifiqui
+
+    //UNIFORM
+    GLuint texUniform = glGetUniformLocation(shaderProgram, "tex0");
+    glUseProgram(shaderProgram);
+    glUniform1i(texUniform, 0);
     
     while (!glfwWindowShouldClose(window))
     {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glBindTexture(GL_TEXTURE_2D, textureID);        
         
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        
-        //glUniform4f(customColorId, 1.0f, 0.0f, 0.0f, 1.0f);
         glDrawArrays(GL_TRIANGLES, 0, 3);
     
         glfwSwapBuffers(window);
         glfwPollEvents();
-
-        Sleep(500);
     }
 
     glDeleteVertexArrays(1, &VAO);
@@ -142,15 +187,4 @@ int main(void)
     
     glfwTerminate();
     return 0;
-}
-
-std::string ReadTextFile(const std::string& fileName)
-{
-    std::ifstream file(fileName);
-    if (!file.is_open()) return "";
-    
-    std::stringstream stringbuffer;
-    stringbuffer << file.rdbuf();
-    file.close();
-    return stringbuffer.str();
 }
